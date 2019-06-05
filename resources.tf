477f7d2f96d2299944e3f1ccc1b2834a309a1e84
Mayowa Babatola <ayowole@gmail.com>
2019-06-05 14:49:57 -0400

    added security groups

--- resources.tf
+++ resources.tf
@@ -18,3 +18,16 @@ resource "aws_subnet" "subnet2"{
     vpc_id = "${aws_vpc.environment-example.id}"
     availability_zone = "us-east-1a"
 }
+
+resource "aws_security_group" "subnetsecurity"{
+    vpc_id = "${aws_vpc.environment-example.id}"
+    ingress {
+        cidr_blocks  = [
+            "${aws_vpc.environment-example.cidr_block}"
+        ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    }
+}
